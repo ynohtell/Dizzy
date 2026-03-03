7bf0a5c96cf57c830e66ce6b5cded3b634f8323d
ynohtell <mayormarkaaron1@gmail.com>
2026-03-03 23:59:05 +0800

    working reader

--- backend/data.py
+++ backend/data.py
@@ -1,7 +1,20 @@
 import os 
 from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
 
-client_id = os.getenv("SPOTIPY_CLIENT_ID")
-print(client_id)
+auth_manager = SpotifyOAuth(
+    client_id=os.getenv("SPOTIFY_CLIENT_ID"),
+    client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
+    redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
+    scope="user-library-read"
+)
+
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " - ", track['name'])
